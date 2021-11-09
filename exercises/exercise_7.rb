0927a4199b1293df2368408fade7a3f956d35dbf
Shuhao Zhang <shuhao.zhang@mail.mcgill.ca>
2021-11-09 00:07:29 -0500

    finish exercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,15 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "test for employees"
+puts @store1.employees.create(first_name: "qwert", last_name: "asdf", hourly_rate: 30).valid?
+puts @store1.employees.create(first_name: "qwert", last_name: "asdf", hourly_rate: 210).valid?
+puts @store1.employees.create(first_name: "", last_name: "asdf", hourly_rate: 50).valid?
+puts @store1.employees.create(first_name: "qwert", last_name: "", hourly_rate: 30).valid?
+puts @store1.employees.create(first_name: "qw", last_name: "asdf", hourly_rate: 50).valid?
+
+puts "test for stores"
+puts Store.create(name: "Ba", annual_revenue: 300000, mens_apparel: true, womens_apparel: true).valid?
+puts Store.create(name: "Bas", annual_revenue: -100, mens_apparel: true, womens_apparel: true).valid?
+puts Store.create(name: "Bas", annual_revenue: 300000, mens_apparel: false, womens_apparel: false).valid?
+puts Store.create(name: "Bas", annual_revenue: 300000, mens_apparel: true, womens_apparel: true).valid?
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,12 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: {only_integer: true, greater_than_or_equal_to: 0}
+  validate :apparel
+
+  def apparel
+    if !mens_apparel && !womens_apparel
+      errors.add(:mens_apparel, "a store must have either mens_apparel or womens_apparel")
+    end
+  end
 end
