4baea6f04ad9b8a291e1937960aecfb91a6c1824
Shuhao Zhang <shuhao.zhang@mail.mcgill.ca>
2021-11-08 22:17:23 -0500

    finish exercise 4

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -7,6 +7,6 @@ puts "----------"
 
 # Your code goes here ...
 @store3 = Store.find_by(id: 3)
-@store3.destory
+@store3.destroy
 
 puts Store.count
--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,23 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+
+Surrey = Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+
+Whistler = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+
+Yaletown = Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+@mens_stores = Store.where(mens_apparel: true)
+
+@mens_stores.each do |store|
+  puts "mens store: #{store.name} has annual revenue #{store.annual_revenue}"
+end
+
+@womens_stores = Store.where(womens_apparel: true)
+
+@womens_stores.each do |store|
+  if store.annual_revenue < 1000000
+    puts "womens store #{store.name} has annual revenue #{store.annual_revenue}"
+  end
+end
